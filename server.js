6de4540947b6689dd98b18ea5a1861726ef1f15c
sidkhuntia <siddharthakhuntia@gmail.com>
2022-01-09 16:01:21 +0530

    mongo update

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
 
-
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 
 const indexRouter = require('./routes/index');
 
@@ -14,7 +16,7 @@ app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/library',{useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:270/library',{useNewUrlParser: true});
 const db = mongoose.connection;
 db.on('error', error => console.error(error));
 db.once('open', () => console.log('Connected to MongoDB'));
