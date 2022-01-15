34b426c23fbbc64bfe31c21a283bbb20e290299a
sidkhuntia <siddharthakhuntia@gmail.com>
2022-01-15 17:52:47 +0530

    Add css changes and filepond

--- public/scripts/fileupload.js
+++ public/scripts/fileupload.js
@@ -1,12 +1,23 @@
-FilePond.registerPlugin(
+const rootStyles = window.getComputedStyle(document.documentElement);
+if (rootStyles.getPropertyValue("--book-cover-width-large") != null && rootStyles.getPropertyValue("--book-cover-width-large") != "") {
+  ready();
+}
+else{
+    document.getElementById("main-css").addEventListener("load", ready);
+}
+function ready() {
+    const coverWidth = parseFloat(rootStyles.getPropertyValue("--book-cover-width-large"));
+    const coverAspectRatio = parseFloat(rootStyles.getPropertyValue("--book-cover-aspect-ratio"));
+    const coverHeight = coverWidth / coverAspectRatio;
+    FilePond.registerPlugin(
     FilePondPluginFileEncode,
     FilePondPluginImagePreview,
     FilePondPluginImageResize
-
-)
-FilePond.setOptions({
-    stylePanelAspectRatio: 1,
-    imageResizeTargetHeight: 250,
-    imageResizeTargetWidth: 250
-})
-FilePond.parse(document.body)
+  );
+  FilePond.setOptions({
+    stylePanelAspectRatio: 1/coverAspectRatio,
+    imageResizeTargetHeight: coverHeight,
+    imageResizeTargetWidth: coverWidth,
+  });
+  FilePond.parse(document.body);
+}
